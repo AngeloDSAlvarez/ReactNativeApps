082f79a93598be81d4c8790f4da671c01db9d8c7
AngeloDSAlvarez <angelodaniel1221@gmail.com>
2024-10-27 17:22:17 -0300

    useEffect, useMemo and useRef

--- Hooks/App.js
+++ Hooks/App.js
@@ -1,15 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo, useRef } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App(){
 
-  const [nome, setNome] = useState('Ângelo');
+  const [nome, setNome] = useState('');
   const [input, setInput] = useState('');
+  const nomeInput = useRef(null); //referencia de obj no app
+
+  //Component DidMount
+  useEffect( () => {
+
+    async function getStorage(){
+      const nomeStorage = await AsyncStorage.getItem('nomes');
+
+      if (nomeStorage !== null) {
+        setNome(nomeStorage);
+      }
+    } 
+
+    getStorage();
+
+  }, [])/
+
+  //Component DidUpdate
+  useEffect( ()=> {
+
+    async function saveStorage() {
+      await AsyncStorage.setItem('nomes', nome);
+    }
+    saveStorage();
+
+  }, [nome] )
 
   function alteraNome() {
     setNome(input);
     setInput('');
   }
+
+  function novoNome() {
+    //uso do ref para referenciar alguma coisa
+    nomeInput.current.focus();
+  }
+
+  // fazer isso para evitar renderizações desnecessárias
+  const letrasNome = useMemo( () => nome.length, [nome] );
+  console.log(letrasNome);
 
   return (
     <View style={styles.container}>
@@ -18,13 +54,20 @@ export default function App(){
       placeholder='Seu nome'
       value={input}
       onChangeText={ (text) => setInput(text) }
+      ref={nomeInput}
       />
       <TouchableOpacity style={styles.btn} onPress={alteraNome}>
         <Text style={styles.btnText}>Altera nome</Text>
       </TouchableOpacity>
 
       <Text style={styles.texto}> {nome} </Text>
-    </View>
+
+      <Text style={styles.texto}> Tem {letrasNome} letras</Text>
+
+      <TouchableOpacity onPress={novoNome}>
+        <Text>Novo nome</Text>
+      </TouchableOpacity>
+    </View> 
   );
 }
 
