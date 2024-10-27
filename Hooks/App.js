acf74ab2846d54f77635fb769b630bcc5b363fe5
AngeloDSAlvarez <angelodaniel1221@gmail.com>
2024-10-27 16:45:33 -0300

    Simple use of hook

---
+++ Hooks/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+
+export default function App(){
+
+  const [nome, setNome] = useState('Ângelo');
+  const [input, setInput] = useState('');
+
+  function alteraNome() {
+    setNome(input);
+    setInput('');
+  }
+
+  return (
+    <View style={styles.container}>
+
+      <TextInput 
+      placeholder='Seu nome'
+      value={input}
+      onChangeText={ (text) => setInput(text) }
+      />
+      <TouchableOpacity style={styles.btn} onPress={alteraNome}>
+        <Text style={styles.btnText}>Altera nome</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.texto}> {nome} </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 15,
+  },
+  texto: {
+    color: '#000',
+    fontSize: 35,
+  },
+  btn: {
+    backgroundColor: '#222',
+  },
+  btnText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 22,
+  }
+})
